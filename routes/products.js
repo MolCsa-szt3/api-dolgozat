a256cd039150ec45a4938cd781fc3c8fd038bb5b
szt3molcsa <szt3molcsa@vasvari.org>
2025-01-15 09:34:45 +0100

    get all works

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,17 @@ import { dbAll, dbGet, dbRun } from "../data/database.js"
 
 const router = express.Router()
 
-//
+//GET all
+router.get("/", async (req,res)=> {
+    try{
+        const rows = await dbAll("SELECT * FROM products")
+        res.status(200).json(rows)
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({message: err.message})
+    }
+})
 
 
 
